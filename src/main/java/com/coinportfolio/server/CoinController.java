50f996540e9e443a2c7fcec8cd798149e62d9c70
Paul <paulfarrell12@gmail.com>
2023-05-14 00:21:09 +0100

    Add H2 database

--- src/main/java/com/coinportfolio/server/CoinController.java
+++ src/main/java/com/coinportfolio/server/CoinController.java
@@ -9,9 +9,10 @@ import com.coinportfolio.server.service.CoinService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.Map;
 
 @RestController
